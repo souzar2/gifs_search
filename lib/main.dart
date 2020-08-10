21a8f35296128d8ac8e966abc738159c34c08323
souzar2 <58992704+souzar2@users.noreply.github.com>
2020-08-10 15:28:24 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:search_gifs/ui/homePage.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
---
+++ lib/ui/homePage.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String _search;
+  int _offSet = 0;
+  Future <Map> _getGifs() async{
+    http.Response response;
+    if(_search == null){
+      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=Q7jEiovVDR2yE9e1MjCOHVAMn5wwj536&limit=25&rating=g");
+    }
+    else{
+      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=Q7jEiovVDR2yE9e1MjCOHVAMn5wwj536&q=$_search&limit=20&offset=$_offSet&rating=g&lang=pt");
+      return json.decode(response.body);
+    }
+  }
+
+
+  @override
+  void initState(){
+    super.initState();
+    _getGifs().then((map){
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
